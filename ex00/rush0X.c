c7e1f72b5cb2f2423736d2c0a20f9044ae28cbf3
Nkosinathi Radebe <nkradebe@c5r8s3.wethinkcode.co.za>
2020-07-15 08:39:46 +0200

    Rush0X

---
+++ ex00/rush0X.c
@@ -0,0 +1,21 @@
+void		rush0X(int	x, int	y)
+{
+	int		a;
+
+	int		b;
+
+	a	=	0;
+	while(a <=	x)
+	{
+		b = 0;
+		while(b	<=	y)
+		{
+			ft_putchar('*');
+			b++;
+		}
+		ft_putchar('\n');	
+		a++;
+	}
+
+}
+
